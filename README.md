68757bb14d0f84173d20c079f1cb8107da96f64d
Steve-design <smzalendo31@gmail.com>
2019-08-26 13:43:18 +0300

    add README.md content to the file

---
+++ README.md
@@ -0,0 +1,30 @@
+# DELANI STUDIO
+#### By **Stephen Mwanza**
+## Description
+Delani studio is a one stop learning area for jQuery and Bootstrap for coding students.
+## BDD
+| Behavior  |      Input       |  Output
+|-------------|----------------|---------------|
+|The browser loads|User input details|Alert message appears
+## Setup/Installation Requirements
+One requires an internet browser in order to view the website oneline.
+For anyone to practice with Delani studio, you must clone or download the repository in the link below to your local machine, into your editor either in code . or atom . to practice.
+## Project live site
+Here is a live link for the project:
+https://Steve-design.github.io/delani-studio
+
+## Technologies Used
+* HTML
+* CSS
+* JQUERY
+* JAVASCRIPT
+* BOOTSTRAP
+## Contact Information
+Reach out to me on smzalendo31@gmail.com
+
+### License
+This project is licensed under MIT licence
+Copyright (c) {2019} 
+
+
+
--- js/scripts.js
+++ js/scripts.js
@@ -1,77 +1,67 @@
 $(document).ready(function(){
     $('.items').click(function () {
-        $(this).children('.hideMe').slideToggle(1200);
+        $(this).children('.hideMe').slideToggle(1500);
         $(this).children('.show').toggle(1200);
       });
-      $(".row4").hover(function(){
-        $(".img4").css({"opacity":"0.2","transition":"3s"});
-        $("#item4").show();
-    },
-    function(){
-        $(".img4").css("opacity","1");
-        $("#item4").hide();
-    });
-    $(".row3").hover(function(){
-        $(".img3").css({"opacity":"0.2","transition":"3s"});
-        $("#item3").show();
-    },
-    function(){
-        $(".img3").css("opacity","1")
-        $("#item3").hide();
-    });
-    $(".row2").hover(function(){
-        $(".img2").css({"opacity":"0.2","transition":"3s"});
-        $("#item2").show();
-    },
-    function(){
-        $(".img2").css("opacity","1")
-        $("#item2").hide();
-    });
-    $(".row1").hover(function(){
-        $(".img1").css({"opacity":"0.2","transition":"3s"});
-        $("#item1").show();
-    },
-    function(){
-        $(".img1").css("opacity","1")
-        $("#item1").hide();
-    });
-    $(".row5").hover(function(){
-        $(".img5").css({"opacity":"0.2","transition":"3s"});
-        $("#item5").show();
-    },
-    function(){
-        $(".img5").css("opacity","1")
-        $("#item5").hide();
-    });
-    $(".row6").hover(function(){
-        $(".img6").css({"opacity":"0.2","transition":"3s"});
-        $("#item6").show();
-    },
-    function(){
-        $(".img6").css("opacity","1")
-        $("#item6").hide();
-    });
-    $(".row7").hover(function(){
-        $(".img7").css({"opacity":"0.2","transition":"3s"});
-        $("#item7").show();
-    },
-    function(){
-        $(".img7").css("opacity","1")
-        $("#item7").hide();
-    });
-    $(".row8").hover(function(){
-        $(".img8").css({"opacity":"0.2","transition":"3s"});
-        $("#item8").show();
-    },
-    function(){
-        $(".img8").css("opacity","1")
-        $("#item8").hide();
-    });
+    
+      
 
      $('form#sendForm').submit(function(){
       var name = $('#nameDetails').val();
       var pass = $('#email').val();
       var mess = $('#content').val();
       alert("Hello " +name+ " we have received your message and we are working on it. Thank you for reaching out to us.");
+     });
     });
-  });
+    $(document).ready(function(){
+      $(".img4").mouseenter(function(){
+      $("#set4").show();
+      }).mouseleave(function(){
+      $("#set4").hide();
+      });
+       
+      $(".img3").mouseenter(function(){
+      $("#set3").show();
+      }).mouseleave(function(){
+      $("#set3").hide();
+      });
+       
+      $(".img2").mouseenter(function(){
+      $("#set2").show();
+      }).mouseleave(function(){
+      $("#set2").hide();
+      });
+       
+      $(".img1").mouseenter(function(){
+      $("#set1").show();
+      }).mouseleave(function(){
+      $("#set1").hide();
+      });
+      });
+      
+    $(document).ready(function(){
+      $(".img5").mouseenter(function(){
+      $("#set5").show();
+      }).mouseleave(function(){
+      $("#set5").hide();
+      });
+       
+      $(".img6").mouseenter(function(){
+      $("#set6").show();
+      }).mouseleave(function(){
+      $("#set6").hide();
+      });
+       
+      $(".img7").mouseenter(function(){
+      $("#set7").show();
+      }).mouseleave(function(){
+      $("#set7").hide();
+      });
+       
+      $(".img8").mouseenter(function(){
+      $("#set8").show();
+      }).mouseleave(function(){
+      $("#set8").hide();
+      });
+      });
+    
