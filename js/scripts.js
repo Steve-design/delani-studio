13e61fca4d59c772e1cedef420d6c6b2fa21cb3d
Steve-design <smzalendo31@gmail.com>
2019-08-26 08:10:58 +0300

    completes the form by creating an alert message once the submit button is clicked

--- js/scripts.js
+++ js/scripts.js
@@ -3,4 +3,16 @@ $(document).ready(function(){
         $(this).children('.hideMe').slideToggle(1200);
         $(this).children('.show').toggle(1200);
       });
+      //$('.card').hover(function(event){
+       // $(".card").hover(function () {
+        //  $(this).children(".card-1").fadeToggle('fast', "linear");
+        //});
+        //event.preventDefault();
+     // });
+     $('form#sendForm').submit(function(){
+      var name = $('#nameDetails').val();
+      var pass = $('#email').val();
+      var mess = $('#content').val();
+      alert("Hello " +name+ " we have received your message and we are working on it. Thank you for reaching out to us.");
+    });
     });
